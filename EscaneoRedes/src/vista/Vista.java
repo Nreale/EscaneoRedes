c98eac0fa6743a3c7bdbe87eb20494f8d6a5c516
Nreale <nahuel.reale.et36@gmail.com>
2026-09-10 15:54:12 -0300

    Parte Inicial de la vista

--- EscaneoRedes/src/vista/Vista.java
+++ EscaneoRedes/src/vista/Vista.java
@@ -1,5 +1,7 @@
 package vista;
-
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,9 +9,39 @@ import javax.swing.*;
 
 public class Vista extends JFrame implements ActionListener{
 	
+	private JTextField txtIpInicio;
+    private JTextField txtIpFin;
+    private JTextField txtTimeout;
+    private JButton btnEscanear;
+    private JButton btnLimpiar;
+    private JButton btnGuardar;
+    private JTextArea txtResultados;
+    private JProgressBar barraProgreso;
+	
 	public Vista() {
+		setTitle("Escaner de Red");
 		setSize(750, 450);
-		setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setLayout(new BorderLayout(10, 10));
+        
+        
+        JPanel panelFormulario = new JPanel(new GridLayout(3, 2, 5, 5));
+        panelFormulario.setBorder(BorderFactory.createTitledBorder("Configuración del Escaneo"));
+        
+        panelFormulario.add(new JLabel("IP Inicial:"));
+        txtIpInicio = new JTextField(""); 
+        panelFormulario.add(txtIpInicio);
+        
+        panelFormulario.add(new JLabel("IP Final:"));
+        txtIpFin = new JTextField("");
+        panelFormulario.add(txtIpFin);
+
+        panelFormulario.add(new JLabel("Timeout (ms):"));
+        txtTimeout = new JTextField("1000");
+        panelFormulario.add(txtTimeout);
+        
+        add(panelFormulario, BorderLayout.NORTH);
+        setVisible(true);
 	}
 	
 	
